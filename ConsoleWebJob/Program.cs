d04757b56581b1b045dc87b20a8416f3048162f1
Allen Lyons <allen.lyons@turner.com>
2016-10-04 13:14:11 -0400

    web job test

---
+++ ConsoleWebJob/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWebJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running here part 2.");
+            Debug.WriteLine("This is a debug message.");
+
+        }
+    }
+}
