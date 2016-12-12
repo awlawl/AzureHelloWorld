51060eaa80382ef28db36675940929b6261a3820
Allen Lyons <allen.lyons@turner.com>
2016-12-12 15:51:13 -0500

    web job test

--- ConsoleWebJob/Program.cs
+++ ConsoleWebJob/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -11,7 +12,18 @@ namespace ConsoleWebJob
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Running here part 3.");
+            string testConfigSetting = ConfigurationManager.AppSettings["TestConfigSetting"];
+
+            if (string.IsNullOrEmpty(testConfigSetting))
+            {
+                Console.WriteLine("The config value was empty.");
+            }
+            else
+            {
+                Console.WriteLine("The config value is:" + testConfigSetting);
+            }
+
+            Console.WriteLine("Running here part 3."); //this is the one that you can see in the webjob logs
             Debug.WriteLine("This is a debug message.");
 
         }
